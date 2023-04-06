ada716c59e4df7f5cef2b19f8eba33f426e0e38e
Omar Ammura <omarammoralm10@gmail.com>
2023-04-07 00:47:55 +0300

    Components and databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { Warningalert } from './warningAlert/warningAlert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Warningalert,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
